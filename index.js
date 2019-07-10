e5872cabc1b8ec9530aa58cfc3b0de1e53dd3581
j66666 <2492601436@qq.com>
2019-07-10 19:10:03 +0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+const fs = require('fs')
 const { exec } = require('child_process')
-exec(`git add . && git commit -m "update" && git push origin master`, err => err && console.log(err))
+const schedule = require('node-schedule');
+
+const commit = () => {
+    exec(`git add . && git commit -m "update" && git push origin master`, err => err && console.log(err))
+}
+
+const scheduleCronstyle = () => {
+    schedule.scheduleJob('*/2 * * * *', () => {
+        fs.readFile('./record.txt', (err, data) => {
+            if (err) {
+                console.log('read err', err)
+            } else {
+                fs.writeFile('./record.txt', `${data}\n${new Date()}`, err => {
+                    if (err) {
+                        console.log('write err', err)
+                    } else {
+                        commit()
+                    }
+                })
+            }
+        })
+    });
+}
+
+scheduleCronstyle();
